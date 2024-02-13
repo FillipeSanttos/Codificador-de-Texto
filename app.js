a4c27469a5a2c1725d0f0f4072dad5dd6c0df724
Fillipe Santtos <fillipesanttos@proton.me>
2024-02-13 16:50:21 -0300

    Corrigir código, Refatorar código

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-
-
-var validacao__texto = /^[a-z ]*$/;
-
-
+var validacao__texto = /^[a-z ]*$/; //Variável que incluí apenas letras minúsculas, sem acentos.
 
 function criptografar__texto(){
-    let frase = document.querySelector('textarea').value;
-    if (validacao__texto.test(frase)) { // se o texto corresponde à expressão
-        console.log("o conteudo esta correto"); 
-        var botao = document.querySelector(".botoes__copiar");                  //ATIVAR O BOTAO COPIAR
-        botao.removeAttribute("hidden");                                        //ATIVAR O BOTAO COPIAR
-        botao.removeAttribute("disable");                                       //ATIVAR O BOTAO COPIAR
-        var botao = document.querySelector(".botoes__inverter");                  //ATIVAR O BOTAO COPIAR
-        botao.removeAttribute("hidden");                                        //ATIVAR O BOTAO COPIAR
-        botao.removeAttribute("disable");                                       //ATIVAR O BOTAO COPIAR
+    let frase = document.querySelector('textarea').value; //Selecionar área digitada
+    if (validacao__texto.test(frase)) { // Verificar se o texto digitado é válido
+
+        //ATIVAR O BOTAO COPIAR E INVERTER
+        var botao = document.querySelector(".botoes__copiar");                  
+        botao.removeAttribute("hidden");                                        
+        botao.removeAttribute("disable");                                       
+        var botao = document.querySelector(".botoes__inverter");                  
+        botao.removeAttribute("hidden");                                        
+        botao.removeAttribute("disable");                                      
         
+        //Codificar frases
         var frase__criptografada = frase.replace(/e/g, "enter");
         var frase__criptografada = frase__criptografada.replace(/i/g, "imes");
         var frase__criptografada = frase__criptografada.replace(/a/g, "ai");
         var frase__criptografada = frase__criptografada.replace(/o/g, "ober");
         var frase__criptografada = frase__criptografada.replace(/u/g, "ufat");
- 
-
-        console.log(frase__criptografada);
-
-
+         
         var mensagem__resolucao = document.querySelector(".texto__resposta")
         mensagem__resolucao.innerHTML = frase__criptografada;
         var elemento = document.querySelector(".texto__resposta");
         elemento.style.backgroundImage = "none";
-
-
     } 
-    else { // se o texto não corresponde à expressão
+    else { // Se o texto digitado não for válido
         console.log("o conteudo possuí caracteres invalidos"); 
         var mensagem__erro = document.querySelector(".texto__resposta")
         mensagem__erro.innerHTML = "Caracteres inválidos digitados! <br> <strong class=\"subtexto_resposta\"> Tente novamente. </strong>";
@@ -40,9 +32,6 @@ function criptografar__texto(){
         elemento.style.backgroundImage = "none";
     }
 }
-
-
-
 
 function descriptografar__texto(){
     let frase = document.querySelector('textarea').value;
@@ -69,7 +58,7 @@ function descriptografar__texto(){
         var elemento = document.querySelector(".texto__resposta");
         elemento.style.backgroundImage = "none";
 
-     
+
     }  
     else { // Se o texto digitado não for válido
         console.log("o conteudo possuí caracteres invalidos"); 
